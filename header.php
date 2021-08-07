f9a45fa97a8a144111f34a453b9df549843c1b19
Analogman <dennis.salazar.castro@gmail.com>
2021-08-07 09:56:08 -0600

    Conversion extensiones

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
     <ul class="navbar-nav">
         <li class="nav-item"><a class="nav-link" href="index.php">Inicio</a></li>  
-        <li id= "item" class="nav-item"><a class="nav-link" href="quiensomos.html">¿Quienes Somos?</a></li>
-        <li class="nav-item"><a class="nav-link" href="serviciosonline.html">Servicios Online</a></li>
-        <li class="nav-item"><a class="nav-link" href="potcast.html">Podcast</a></li>
-        <li class="nav-item"><a class="nav-link" href="galeria.html">Galería</a></li>
-        <li class="nav-item"><a class="nav-link" href="reflex.html">Reflexiones</a></li>
-        <li class="nav-item"><a class="nav-link" href="noticias.html">Noticias</a></li>
-        <li class="nav-item"><a class="nav-link" href="contacto.html">Contáctanos</a></li>
+        <li id= "item" class="nav-item"><a class="nav-link" href="quiensomos.php">¿Quienes Somos?</a></li>
+        <li class="nav-item"><a class="nav-link" href="serviciosonline.php">Servicios Online</a></li>
+        <li class="nav-item"><a class="nav-link" href="potcast.php">Podcast</a></li>
+        <li class="nav-item"><a class="nav-link" href="galeria.php">Galería</a></li>
+        <li class="nav-item"><a class="nav-link" href="reflex.php">Reflexiones</a></li>
+        <li class="nav-item"><a class="nav-link" href="noticias.php">Noticias</a></li>
+        <li class="nav-item"><a class="nav-link" href="contacto.php">Contáctanos</a></li>
         
         <style>
             .dropdown-item{
